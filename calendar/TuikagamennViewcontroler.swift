06f665a05ac636d42e93501c9f3a6bbc3aa8bbbe
issei fukumura <fukumura.issei@gmail.com>
2021-05-11 16:57:15 +0900

    textfield を選択したときにキーボードを閉じれるようにした。 同じ名前でも登録できるようにした。

--- calendar/TuikagamennViewcontroler.swift
+++ calendar/TuikagamennViewcontroler.swift
@@ -13,10 +13,10 @@ class TuikagamennViewcontroler: UIViewController {
     @IBOutlet var notificationDatePicker: UIDatePicker!
     @IBOutlet var kadaiTextField: UITextField!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     @IBAction func save() {
         
@@ -27,30 +27,47 @@ class TuikagamennViewcontroler: UIViewController {
         content.title = kadai + "をやりましょう"
         content.body = kadai + "の提出期限は" + String(describing: deadlineDate) + "です"
         let timeInterval: TimeInterval = notificationDate.timeIntervalSinceNow
-        let trigger: UNTimeIntervalNotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
         
-        let request: UNNotificationRequest = UNNotificationRequest(identifier: kadai, content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-        self.navigationController?.popViewController(animated: true)
+        
+        if timeInterval > 0 {
+            let trigger: UNTimeIntervalNotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+            
+            
+            
+            let formatter: DateFormatter = DateFormatter()
+            
+            formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM HH:mm:ss", options: 0, locale: Locale(identifier: "ja_JP"))
+            let dateString: String = formatter.string(from: Date())
+            
+            let request: UNNotificationRequest = UNNotificationRequest(identifier: kadai + dateString, content: content, trigger: trigger)
+            
+            if  !kadai.isEmpty {
+                
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+                self.navigationController?.popViewController(animated: true)
+            }
+         
+        }
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
 
 
 
+
+/*
+ // MARK: - Navigation
  
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
